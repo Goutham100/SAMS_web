15742508fa53d34df7105abad65f01bd8e626ab8
Goutham <gouthamjagadeesh100@gmail.com>
2024-10-13 23:21:32 +0530

    update

---
+++ dist/main3.bd7bd11c.js
@@ -0,0 +1,6 @@
+let display_value = 23;
+function display() {
+    document.getElementById("output").innerHTML = display_value;
+}
+
+//# sourceMappingURL=main3.bd7bd11c.js.map
---
+++ script3.js
@@ -0,0 +1,4 @@
+let display_value = 23;
+
+function display()
+{document.getElementById("output").innerHTML = display_value;}
